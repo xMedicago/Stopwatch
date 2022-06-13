b6bb44bbe68b9610e1d4cd4dcd8fefe6d34ba983
xMedicago <87269586+xMedicago@users.noreply.github.com>
2022-06-13 18:57:51 +0300

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -1,62 +1,66 @@
 import tkinter as tk
 from datetime import datetime
 
-start_time = 0
-after_id = ""
 
-
-def stopwatch():
-    global start_time, after_id
-    after_id = root.after(1000, stopwatch)
-    f_start_time = datetime.fromtimestamp(start_time).strftime("%M:%S")
-    label_1.configure(text=str(f_start_time))
-    start_time += 1
-
-
-def start_button():
-    first_button.grid_forget()
-    second_button.grid(row=1, columnspan=2, sticky="ew")
-    stopwatch()
-
-
-def stop_button():
-    second_button.grid_forget()
-    third_button.grid(row=1, column=0, sticky="ew")
-    fourth_button.grid(row=1, column=1, sticky="ew")
-    root.after_cancel(after_id)
-
-
-def continue_button():
-    third_button.grid_forget()
-    fourth_button.grid_forget()
-    second_button.grid(row=1, columnspan=2, sticky="ew")
-    stopwatch()
-
-
-def reset_button():
-    global start_time
+class Stopwatch:
     start_time = 0
-    label_1.configure(text="00:00")
-    third_button.grid_forget()
-    fourth_button.grid_forget()
-    first_button.grid(row=1, columnspan=2, sticky="ew")
+    after_id = ""
 
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.resizable(False, False)
+        self.root.title("Секундомер")
+        self.photo = tk.PhotoImage(file="stopwatch_46861.png")
+        self.root.iconphoto(False, self.photo)
 
-root = tk.Tk()
-root.resizable(False, False)
-root.title("Секундомер")
-photo = tk.PhotoImage(file="stopwatch_46861.png")
-root.iconphoto(False, photo)
+        self.label_1 = tk.Label(self.root, width=5, font=("Ubuntu", 100), text="00:00")
+        self.label_1.grid(row=0, columnspan=2)
 
-label_1 = tk.Label(root, width=5, font=("Ubuntu", 100), text="00:00")
-label_1.grid(row=0, columnspan=2)
+        self.first_button = tk.Button(self.root, font=("Ubuntu", 30), text="Start", command=self.start_button,
+                                      background="#cd853f",
+                                      relief=tk.RAISED, bd=10)
+        self.second_button = tk.Button(self.root, font=("Ubuntu", 30), text="Stop", command=self.stop_button,
+                                       background="red",
+                                       relief=tk.RAISED, bd=10)
+        self.third_button = tk.Button(self.root, font=("Ubuntu", 30), width=5, text="Continue",
+                                      command=self.continue_button,
+                                      background="yellow", relief=tk.RAISED, bd=10)
+        self.fourth_button = tk.Button(self.root, font=("Ubuntu", 30), width=5, text="Reset", command=self.reset_button,
+                                       background="#AD8AAA",
+                                       relief=tk.RAISED, bd=10)
 
-first_button = tk.Button(root, font=("Ubuntu", 30), text="Start", command=start_button, background="#cd853f", relief=tk.RAISED, bd=10)
-second_button = tk.Button(root, font=("Ubuntu", 30), text="Stop", command=stop_button, background="red", relief=tk.RAISED, bd=10)
-third_button = tk.Button(root, font=("Ubuntu", 30), width=5, text="Continue", command=continue_button, background="yellow", relief=tk.RAISED, bd=10)
-fourth_button = tk.Button(root, font=("Ubuntu", 30), width=5, text="Reset", command=reset_button, background="#AD8AAA", relief=tk.RAISED, bd=10)
+        self.first_button.grid(row=1, columnspan=2, sticky="ew")
 
-first_button.grid(row=1, columnspan=2, sticky="ew")
+    def stopwatch(self):
+        self.after_id = self.root.after(1000, self.stopwatch)
+        f_start_time = datetime.fromtimestamp(self.start_time).strftime("%M:%S")
+        self.label_1.configure(text=str(f_start_time))
+        self.start_time += 1
+
+    def start_button(self):
+        self.first_button.grid_forget()
+        self.second_button.grid(row=1, columnspan=2, sticky="ew")
+        self.stopwatch()
+
+    def stop_button(self):
+        self.second_button.grid_forget()
+        self.third_button.grid(row=1, column=0, sticky="ew")
+        self.fourth_button.grid(row=1, column=1, sticky="ew")
+        self.root.after_cancel(self.after_id)
+
+    def continue_button(self):
+        self.third_button.grid_forget()
+        self.fourth_button.grid_forget()
+        self.second_button.grid(row=1, columnspan=2, sticky="ew")
+        self.stopwatch()
+
+    def reset_button(self):
+        self.start_time = 0
+        self.label_1.configure(text="00:00")
+        self.third_button.grid_forget()
+        self.fourth_button.grid_forget()
+        self.first_button.grid(row=1, columnspan=2, sticky="ew")
+
 
 if __name__ == '__main__':
-    root.mainloop()
+    Stopwatch().root.mainloop()
